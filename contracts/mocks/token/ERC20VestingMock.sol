46e6b391704ac999f3604c476373d5eb953b99d8
Nicolas <lapingarou@gmail.com>
2023-07-14 13:18:35 +0800

    feat: mock contracts

---
+++ contracts/mocks/token/ERC20VestingMock.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20, IERC20} from "../../token/ERC20/ERC20.sol";
+import {ERC20Vesting} from "../../token/ERC20/extensions/ERC20Vesting.sol";
+import {ERC20Wrapper} from "../../token/ERC20/extensions/ERC20Wrapper.sol";
+import {ERC20Votes} from "../../token/ERC20/extensions/ERC20Votes.sol";
+import {SafeERC20} from "../../token/ERC20/utils/SafeERC20.sol";
+
+
+// Escrowed governance requires vesting before withdrawal
+abstract contract EsGovernanceTokenMock is ERC20Votes, ERC20Wrapper, ERC20Vesting {
+  constructor(address token, uint64 vestingDuration) 
+    ERC20Vesting(vestingDuration)
+  {}
+  
+  // Transfer of escrowed tokens is forbidden
+  function transfer(address to, uint256 value) public override returns (bool) {
+    revert("EsGovernanceTokenMock: Transfer Forbidden");
+  }
+  
+  // Withdrawal: ERC20Wrapper param {value} becomes a user schedule, amount withdrawn is defined by the schedule
+  function withdrawTo(address account, uint256 userVestingId) public virtual override returns (bool) {
+    //uint256 userVestingId = value;
+    (uint256 unlockedAmount, uint256 lockedAmount) = _withdraw(account, userVestingId);
+    require(lockedAmount == 0, "EsGovernanceTokenMock: Still Vesting");
+    
+    return super.withdrawTo(account, unlockedAmount);
+  }
+  
+  // overrides
+  function _update(address from, address to, uint256 value) internal virtual override (ERC20Votes, ERC20) {
+    return super._update(from, to, value);
+  }
+  function decimals() public view virtual override (ERC20Wrapper, ERC20) returns (uint8) {
+    return super.decimals();
+  }
+}
+
+
+
+/** ve- kind of lockup model can lock tokens for 1w to 4y to get extra weight
+ * token balance is the virtual balance. real balance of underlying is stored in _realBalances
+ *
+ * the weight is MIN_LOCKUP / MAX_LOCKUP * lockupDuration (e.g lock 1000 CRV for 4y: get 1000 veCRV, for 1w: get 1000*1/208 = ~4.8 veCRV)
+ * Note that this is not the CRV model since durations cannot be extended or averaged etc.
+ */
+abstract contract VeGovernanceTokenMock is ERC20Votes, ERC20Vesting {
+  IERC20 public immutable underlying;
+
+  // Min and max lockup duration in seconds
+  uint64 public constant MAX_LOCKUP = 4 * 365 * 86400; // ~4y
+  uint64 public constant MIN_LOCKUP = 7 * 86400; // 1w
+  
+  // Token deposits are automatically vested, the real deposit balance is stored in an array indexed by the vesting structure id
+  mapping (uint256 => uint256) public vestingUnderlyingBalances;
+  
+  
+  constructor(address underlying_)
+    ERC20Vesting(MAX_LOCKUP)
+  {
+    require(underlying_ != address(0x0), "VeGovernanceTokenMock: Invalid Underlying");
+    underlying = IERC20(underlying_);
+  }
+  
+  
+  // Withdraws fully vested tokens
+  function withdrawTo(address account, uint256 userVestingId) public returns (bool) {
+    (uint256 unlockedAmount, uint256 lockedAmount) = _withdraw(account, userVestingId);
+    require(lockedAmount == 0, "VeGovernanceTokenMock: Still Vesting");
+    
+    // Burn the virtual balance and withdraw the real underlying balance
+    _burn(account, unlockedAmount);
+    uint vestingId = getVestingScheduleId(account, userVestingId);
+    SafeERC20.safeTransfer(underlying, account, vestingUnderlyingBalances[vestingId]);
+  }
+  
+  
+  // Deposit tokens: automatically starts vesting them
+  function depositFor(address account, uint256 value, uint64 lockupDuration) public returns (bool) {
+    require(lockupDuration >= MIN_LOCKUP && lockupDuration <= MAX_LOCKUP, "VeGovernanceTokenMock: Invalid Lockup Duration");
+    
+    uint weightedValue = value * lockupDuration * MIN_LOCKUP / MAX_LOCKUP;
+    // Transfer underlying and mint virtual balance
+    SafeERC20.safeTransferFrom(underlying, account, address(this), value);
+    _mint(account, weightedValue);
+    // automatically start vesting for lockupDuration
+    uint vestingId = _vest(_msgSender(), value, uint64(block.timestamp + lockupDuration - MAX_LOCKUP));
+    vestingUnderlyingBalances[vestingId] = value;
+    
+    return true;
+  }
+  
+  
+  // overrides
+  function _update(address from, address to, uint256 value) internal virtual override (ERC20Votes, ERC20) {
+    return super._update(from, to, value);
+  }
+}
+
+
+/**
+ * Liquidity mining vested rewards
+ * Rewards are deposited in this contract and users can withdraw them after vesting 90d
+ * Rewards can be withdrawn early, in which case there is a penalty and the non unlocked amount is burnt
+ */
+abstract contract MiningRewards is ERC20Vesting {
+  IERC20 public immutable rewardToken;
+  
+  constructor(address rewardToken_) 
+    ERC20Vesting(90 * 86400)
+  {
+    rewardToken = IERC20(rewardToken_);
+  }
+  
+  
+  function redeem(uint userVestingId) public returns (bool) {
+    (uint256 unlockedAmount, uint256 lockedAmount) = _withdraw(msg.sender, userVestingId);
+    // locked amount is ignored and lost, unlocked amount is transferred
+    SafeERC20.safeTransfer(rewardToken, msg.sender, unlockedAmount);
+  }
+  
+}
--- contracts/token/ERC20/extensions/ERC20Vesting.sol
+++ contracts/token/ERC20/extensions/ERC20Vesting.sol
@@ -16,32 +16,33 @@ abstract contract ERC20Vesting is ERC20 {
   /// Event: update vesting duration
   event UpdatedVestingDuration(uint vestingDuration);
   /// Event: Send tokens to user while penalty for early unlock is burnt
-  event Withdraw(address indexed user, uint256 received, uint256 penalty);
+  event Withdraw(address indexed user, uint256 unlockedAmount, uint256 lockedAmount);
 
   /// @notice vesting duration in seconds
-  uint256 private _vestingDuration; 
+  uint64 private _vestingDuration; 
 
   /// @notice Token vesting structure
   struct VestingSchedule {
-    uint128 vestedAmount;
+    uint192 vestedAmount;
     uint64 startTime;
-    // Rserved data field for user defined logic
-    uint64 data;
   }
+  
+  /// @notice Vesting schedules
+  VestingSchedule[] private _vestingSchedules;
 
-  /// @notice Vesting schedule
-  mapping(address => VestingSchedule[]) private _vestingSchedules;
+  /// @notice User vesting schedules
+  mapping(address => uint256[]) private _userVestingSchedules;
 
   /// @notice Total vesting tokens
   uint256 private _totalVestingBalance;
 
-  /// @notice Individual allocation vesting amount
+  /// @notice Individual total vesting balance
   mapping(address => uint256) private _userVestingBalances;
   
   
   
   /// @dev Sets the the value for {vestingDuration}
-  constructor(uint256 vestingDuration_){
+  constructor(uint64 vestingDuration_){
     require(vestingDuration_ > 0, "ERC20Vesting: Zero vesting");
     _vestingDuration = vestingDuration_;
   }
@@ -65,21 +66,47 @@ abstract contract ERC20Vesting is ERC20 {
   
   /// @notice Get number of vesting structs for a user
   function getVestingLength(address user) public view returns (uint256) {
-    return _vestingSchedules[user].length;
+    return _userVestingSchedules[user].length;
   }
   
-  /// @notice Returns data for a given schedule
-  function getVestingSchedule(address user, uint vestingId) 
-    public view returns (uint256 vestedAmount, uint256 startTime, uint64 data)
+  /// @notice Returns data for a vesting schedule
+  /// @param vestedAmount Intial vested amount
+  /// @param startTime Vesting start date
+  /// @param unlockedAmount Amount already vested
+  /// @param lockedAmount Amount remaining to vest
+  /// @dev lockedAmount and unlockedAmount are not indicative of whether
+  function getVestingSchedule(uint vestingId) 
+    public view virtual 
+    returns (uint256 vestedAmount, uint64 startTime, uint256 unlockedAmount, uint256 lockedAmount)
   {
-    VestingSchedule memory vs = _vestingSchedules[user][vestingId];
-    return (uint256(vs.vestedAmount), uint256(vs.startTime), vs.data);
+    require(vestingId < _vestingSchedules.length, "ERC20Vesting: Invalid Schedule");
+    VestingSchedule memory vs = _vestingSchedules[vestingId];
+    vestedAmount  = uint256(vs.vestedAmount);
+    startTime = vs.startTime;
+    (unlockedAmount, lockedAmount) = vestingStatus(vestedAmount, startTime);
   }
+  
+  
+  /// @notice Returns vesting Id 
+  function getVestingScheduleId(address user, uint256 userVestingId) internal view returns (uint256){
+    require(userVestingId < getVestingLength(user), "ERC20Vesting: Invalid userVestingId");
+    return _userVestingSchedules[user][userVestingId];
+  }
+  
+  
+  /// @notice Returns data for a user's vesting schedule
+  function getUserVestingSchedule(address user, uint userVestingId)
+    public view virtual 
+    returns (uint256 vestedAmount, uint64 startTime, uint256 unlockedAmount, uint256 lockedAmount)
+  {
+    return getVestingSchedule(getVestingScheduleId(user, userVestingId));
+  }
+  
   
   /// @notice Update vesting duration
-  /// @param vestingDuration_ New vesting duration, non infinite
-  function _updateVestingDuration(uint256 vestingDuration_) internal virtual {
-    require(vestingDuration_ > 0 && vestingDuration_ < 2**64, "Invalid vesting duration");
+  /// @param vestingDuration_ New vesting duration
+  function _updateVestingDuration(uint64 vestingDuration_) internal virtual {
+    require(vestingDuration_ > 0, "Invalid vesting duration");
     _vestingDuration = vestingDuration_;
     emit UpdatedVestingDuration(_vestingDuration);
   }  
@@ -89,78 +116,74 @@ abstract contract ERC20Vesting is ERC20 {
   
   /// @notice Vest locked token
   /// @param vestingAmount Amount of tokens to vest
-  function vest(uint vestingAmount) public virtual {
+  function vest(uint vestingAmount) public virtual returns (uint userVestingId){
     address sender = _msgSender();
-    require(balanceOf(sender) >= vestingAmount + vestingBalanceOf(sender), "ERC20Vesting: Insufficient Balance to Vest");
-    uint64 data;
-    _vest(sender, vestingAmount, data);
+    _vest(sender, vestingAmount, uint64(block.timestamp));
+    userVestingId = _userVestingSchedules[sender].length - 1;
   }
   
   
   /// @notice Vesting starts the unlock countdown for a user's subset of tokens
-  /// @dev Caller should make sure that the user has enough funds to vest
-  function _vest(address user, uint256 vestingAmount, uint64 data) internal {
-    require(vestingAmount > 0 || vestingAmount < type(uint224).max, "ERC20Vesting: Invalid vesting amount");
-    _vestingSchedules[user].push(VestingSchedule(uint128(vestingAmount), uint64(block.timestamp), data));
+  function _vest(address user, uint256 vestingAmount, uint64 startTime) internal returns (uint vestingId) {
+    require(balanceOf(user) >= vestingAmount + vestingBalanceOf(user), "ERC20Vesting: Insufficient Balance");
+    require(vestingAmount > 0 || vestingAmount < type(uint224).max, "ERC20Vesting: Invalid Amount");
+    require(startTime + _vestingDuration < block.timestamp, "ERC20Vesting: Invalid Time");
+    _vestingSchedules.push(VestingSchedule(uint192(vestingAmount), startTime));
+    vestingId = _vestingSchedules.length - 1;
+    _userVestingSchedules[user].push(_vestingSchedules.length-1);
     _totalVestingBalance += vestingAmount;
     _userVestingBalances[user] += vestingAmount;
     emit Vested(user, vestingAmount);
   }
-  
+
   
   /// @notice User can withdraw tokens once vesting is over
   /// @param user Owner of the vesting structure
-  /// @param vestingId Id of the vesting structure since each user can have several
-  /// @return received Amount of tokens unlocked to send to the user
-  /// @return penalty Amount of tokens to burn as early withdrawal penalty
+  /// @param userVestingId Id of the vesting structure in the user list
+  /// @return unlockedAmount Amount of tokens unlocked
+  /// @return lockedAmount Amount of tokens remaining locked
   /// @dev Doesnt actually send anything, only does accounting and return values to be sent/burnt - doesn't check ownership
-  function _withdraw(address user, uint256 vestingId) internal returns (uint256 received, uint256 penalty){
+  function _withdraw(address user, uint256 userVestingId) internal returns (uint256 unlockedAmount, uint256 lockedAmount){
     uint userVestingLength = getVestingLength(user);
-    require(userVestingLength > vestingId, "Invalid vestingId");
+    uint256 vestedAmount;
+    uint64 startTime;
+    (vestedAmount, startTime, unlockedAmount, lockedAmount) = getUserVestingSchedule(user, userVestingId);
 
-    uint64 startTime = _vestingSchedules[user][vestingId].startTime;
-    uint256 vestedAmount = uint256(_vestingSchedules[user][vestingId].vestedAmount);
-    
-    (received, penalty) = vestingSchedule(vestedAmount, startTime);
-
-    // remove struct from list
-    if(vestingId < userVestingLength - 1){
-      _vestingSchedules[user][vestingId] = _vestingSchedules[user][userVestingLength - 1];
+    // remove from user list
+    if(userVestingId < userVestingLength - 1){
+      _userVestingSchedules[user][userVestingId] = _userVestingSchedules[user][userVestingLength - 1];
     }
-    _vestingSchedules[user].pop();
+    _userVestingSchedules[user].pop();
     
-    _totalVestingBalance -= uint256(vestedAmount);
-    _userVestingBalances[user] -= uint256(vestedAmount);
-    emit Withdraw(user, received, penalty);
+    _totalVestingBalance -= vestedAmount;
+    _userVestingBalances[user] -= vestedAmount;
+    emit Withdraw(user, unlockedAmount, lockedAmount);
   }
+
   
   
   /// @notice Calculates the amount that can be received
   /// @param vestedAmount Amount of tokens vested
   /// @param startTime End of vesting timestamp
-  /// @return received Amount of tokens unlocked to send to the user
-  /// @return remaining Amount of tokens remaining locked
-  /// @dev Two simple use cases: withdrawal forbidden if remaining > 0, or remaining is burnt when early unlock
-  function vestingSchedule(uint256 vestedAmount, uint64 startTime) public view returns (uint256 received, uint256 remaining) {
+  /// @return unlockedAmount Amount of tokens unlocked
+  /// @return lockedAmount Amount of tokens remaining locked
+  function vestingStatus(uint256 vestedAmount, uint64 startTime) public view returns (uint256 unlockedAmount, uint256 lockedAmount) {
     if (block.timestamp >= startTime + _vestingDuration){
-      received = vestedAmount;
+      unlockedAmount = vestedAmount;
     }
     else if (block.timestamp <= startTime){
-      remaining = vestedAmount;
+      lockedAmount = vestedAmount;
     }
     else {
-      (received, remaining) = _vestingSchedule(vestedAmount, startTime);
+      (unlockedAmount, lockedAmount) = _vestingStatus(vestedAmount, startTime);
     }
   }
   
   
-  /// @notice Calculate the actual received/penalty amounts for non trivial results, default is linear unlock
-  function _vestingSchedule(uint256 vestedAmount, uint64 startTime) internal view virtual returns (uint256 received, uint256 remaining) {
-    received = (vestedAmount * (block.timestamp - startTime)) / _vestingDuration;
-    remaining = vestedAmount - received;
+  /// @notice Calculate the actual unlocked/locked amounts for non trivial results, default is linear unlock
+  function _vestingStatus(uint256 vestedAmount, uint64 startTime) internal view virtual returns (uint256 unlockedAmount, uint256 lockedAmount) {
+    unlockedAmount = (vestedAmount * (block.timestamp - startTime)) / _vestingDuration;
+    lockedAmount = vestedAmount - unlockedAmount;
   }
-  
-  
-  //////// OVERRIDES
   
 }
